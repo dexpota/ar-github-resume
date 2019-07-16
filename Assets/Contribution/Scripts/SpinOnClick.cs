049253ba2c5564674a8d51ce77f5168d55eba592
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-07-16 20:59:44 +0200

    First commit.

---
+++ Assets/Contribution/Scripts/SpinOnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinOnClick : MonoBehaviour
+{
+    private Animator animator;
+    private static readonly int Spin = Animator.StringToHash("Spin");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0)) {
+            animator.SetTrigger(Spin);
+        }
+            
+    }
+}
